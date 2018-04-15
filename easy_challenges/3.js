4ca6f9637bd0c902e9179aef70bd7d8ac38ae8ff
GeoFro <georgeharoldfrost@gmail.com>
2018-04-16 00:32:44 +1000

    Challenge 3

---
+++ easy_challenges/3.js
@@ -0,0 +1,15 @@
+// Have the function FirstReverse(str) take the str parameter being passed and return the string in reversed order.
+// For example: if the input string is "Hello World and Coders" then your program should return the string sredoC dna dlroW olleH.
+
+function FirstReverse(str) {
+
+  // code goes here
+  var splitString = str.split("");
+  var reverse = splitString.reverse();
+  var joinArray = reverse.join("");
+  return joinArray;
+
+}
+
+// keep this function call here
+FirstReverse(readline());
